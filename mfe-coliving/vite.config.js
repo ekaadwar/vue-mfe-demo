f4157c3dc6608151a94490779f2ce64a3b483e1c
Eka Fajhari Adwar <ekafajhariadwar@gmail.com>
2026-01-26 11:28:52 +0700

    install dependencies vite-plugin-federation

--- mfe-coliving/vite.config.js
+++ mfe-coliving/vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import federation from '@originjs/vite-plugin-federation'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    federation({
+      name: 'coliving',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './ColivingApp': './src/ColivingApp.vue'
+      },
+      shared: ['vue']
+    })
+  ],
+  build: {
+    target: 'esnext'
+  },
+  server: {
+    port: 5002,
+    strictPort: true,
+    cors: true
+  }
 })
