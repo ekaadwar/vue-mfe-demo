6950d584a6f3268c38bb2a1e5fba137c57ab8fec
Eka Fajhari Adwar (P) <eka_fa.p@bi.go.id>
2026-01-30 10:43:06 +0700

    FIX | ./host-shell/style.css : perbaikan width | ./HotelPage.vue : import remote component hotel menggunakan defineAsyncComponent dan menampilkan nya dalam bentuk component HotelApp

--- host-shell/src/views/HotelPage.vue
+++ host-shell/src/views/HotelPage.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1 style="color: #495369">Hotel Page</h1>
+  <HotelApp/>
 </template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue';
+
+const HotelApp = defineAsyncComponent(()=> import('hotels/HotelApp'))
+</script>
